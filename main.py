776d6d62bd1467e36499b9dc67c79023281dd1e7
vinitthakar <32064853+vinitthakar@users.noreply.github.com>
2017-10-20 14:37:05 -0300

    attempt

---
+++ main.py
@@ -0,0 +1,23 @@
+def find _longest_word():
+    gfile = open('book1.txt','r')
+    longg = ' '
+for line in gfile :
+    if len(line)>len(longg)
+       longg = line
+return longgdict.txt
+def find_longest_word():
+    gfile = open('book2.txt','r')
+    longg = ' '
+for  line in gfile :
+     if len(line)>len(longg)
+        longg = line
+return longgdict.txt
+def find_longest_word():
+    gfile = open('book3'.txt,'r')
+    longg = ' '
+for line in gfile :
+    if len(line)> len(longg)
+       longg = line
+return longgdict.txt
+longest_word = words.split()
+find_longest_word()
